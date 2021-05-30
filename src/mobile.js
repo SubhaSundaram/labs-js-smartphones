f9454136be90a43cd487c54f0c1e92bdc35ca6a8
SubhaSundaram <subhasundar664@gmail.com>
2021-05-30 14:29:22 +0530

    final commit

--- src/mobile.js
+++ src/mobile.js
@@ -1,13 +1,102 @@
 //SmartPhone
-
-
+class SmartPhone {
+  constructor(ram, batteryPower, price) {
+    this.ram = ram
+    this.batteryPower = batteryPower
+    this.price = price
+  }
+  displayFeatures() {
+    return this.ram + '\n' + this.batteryPower + '\n' + this.price
+  }
+  remainingAmount(priceIHave) {
+    this.price = this.price - priceIHave
+  }
+}
 
 // AndroidPhone
 
-
-
+class AndroidPhone extends SmartPhone {
+  constructor(modelName, ram, batteryPower, price) {
+    super(ram, batteryPower, price)
+    this.modelName = modelName
+  }
+  displayFeatures() {
+    super.displayFeatures()
+    return (
+      this.modelName +
+      '\n' +
+      this.ram +
+      '\n' +
+      this.batteryPower +
+      '\n' +
+      this.price
+    )
+  }
+  remainingAmount(priceIHave) {
+    this.price = priceIHave - this.price
+    if (this.price <= 0) return 'Customer has no more amount'
+    else
+      return (
+        'Customer has remaining Rs.' +
+        this.price +
+        ' after buying the android phone'
+      )
+  }
+}
 // IPhone
-
-
+class IPhone extends SmartPhone {
+  constructor(seriesName, ram, batteryPower, price) {
+    super(ram, batteryPower, price)
+    this.seriesName = seriesName
+  }
+  displayFeatures() {
+    // super.displayFeatures()
+    return (
+      this.seriesName +
+      '\n' +
+      this.ram +
+      '\n' +
+      this.batteryPower +
+      '\n' +
+      this.price
+    )
+  }
+  remainingAmount(priceIHave) {
+    this.price = priceIHave - this.price
+    console.log(priceIHave)
+    if (this.price == 0) return 'Customer has no more amount'
+    else if (this.price < 0)
+      return "Customer can't able to buy a phone due to insufficient amount"
+    else
+      return (
+        'Customer has remaining Rs.' + this.price + ' after buying an iphone'
+      )
+  }
+}
 
 //MobileCampus
+
+class MobileCampus {
+  constructor(NoOfMobiles, NoOfAndroidMobiles, NoOfIPhoneMobiles) {
+    this.NoOfMobiles = NoOfMobiles
+    this.NoOfAndroidMobiles = NoOfAndroidMobiles
+    this.NoOfIPhoneMobiles = NoOfIPhoneMobiles
+  }
+  changeMobileAvailabilityNumber(count, type) {
+    this.NoOfMobiles = this.NoOfMobiles - count
+    if (type === 'android') {
+      this.NoOfAndroidMobiles = this.NoOfAndroidMobiles - count
+      return 'Available android mobiles are ' + this.NoOfAndroidMobiles
+    }
+    if (type === 'iphone') {
+      this.NoOfIPhoneMobiles = this.NoOfIPhoneMobiles - count
+      return 'Available iphone mobiles are ' + this.NoOfIPhoneMobiles
+    }
+  }
+}
+
+let iphones = new IPhone('Redmi', '4GB', '4000MaH', '20000')
+iphones.displayFeatures()
+console.log(iphones.remainingAmount(30000))
+let phones = new MobileCampus(1000, 700, 300)
+phones.changeMobileAvailabilityNumber(300, 'android')
